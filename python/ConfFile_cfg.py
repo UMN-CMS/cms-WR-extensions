03cb890fecd0b4ed01c2af14cca5a5ca639d3b60
Nicole Ruckstuhl <nicole.manuela.ruckstuhl@cern.ch>
2017-05-10 14:46:50 +0200

    maxevent parsing

--- python/ConfFile_cfg.py
+++ python/ConfFile_cfg.py
@@ -3,13 +3,14 @@ from FWCore.ParameterSet.VarParsing import VarParsing
 
 options = VarParsing ('analysis')
 
+options.maxEvents = -1
 options.parseArguments()
 
 process = cms.Process("Demo")
 
 process.load("FWCore.MessageService.MessageLogger_cfi")
 
-process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(-1) )
+process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(options.maxEvents) )
 
 process.source = cms.Source ("PoolSource",
 	  fileNames = cms.untracked.vstring (options.inputFiles),
