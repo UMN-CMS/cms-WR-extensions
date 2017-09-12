0a0404caec0633b2d4d18d5a941a0a73dda95ced
Andrew Evans <aevans@physics.umn.edu>
2017-09-12 01:06:01 -0500

    building skimmer

---
+++ plugins/skimEvents.cc
@@ -0,0 +1,192 @@
+// -*- C++ -*-
+//
+// Package:    ExoAnalysis/skimEvents
+// Class:      skimEvents
+// 
+/**\class skimEvents skimEvents.cc ExoAnalysis/skimEvents/plugins/skimEvents.cc
+
+ Description: [one line class summary]
+
+ Implementation:
+     [Notes on implementation]
+*/
+//
+// Original Author:  Andrew Evans
+//         Created:  Tue, 05 Sep 2017 15:23:17 GMT
+//
+//
+
+
+// system include files
+#include <memory>
+#include <vector>
+#include <iostream>
+
+// user include files
+#include "FWCore/Framework/interface/Frameworkfwd.h"
+#include "FWCore/Framework/interface/stream/EDFilter.h"
+
+#include "FWCore/Framework/interface/Event.h"
+#include "FWCore/Framework/interface/MakerMacros.h"
+
+#include "FWCore/ParameterSet/interface/ParameterSet.h"
+#include "FWCore/Utilities/interface/StreamID.h"
+
+#include "DataFormats/PatCandidates/interface/Jet.h"
+#include "DataFormats/PatCandidates/interface/Muon.h"
+#include "SimDataFormats/GeneratorProducts/interface/GenEventInfoProduct.h"
+#include "FWCore/Utilities/interface/InputTag.h"
+#include "FWCore/Utilities/interface/EDGetToken.h"
+#include "FWCore/ServiceRegistry/interface/Service.h"
+#include "CommonTools/UtilAlgos/interface/TFileService.h"
+
+//ROOT includes
+#include "TH1D.h"
+
+//
+//local includes
+#include "ExoAnalysis/cms-WR-extensions/interface/eventBits.h"
+#include "ExoAnalysis/cms-WR-extensions/interface/eventHistos.h"
+#include "ExoAnalysis/cms-WR-extensions/interface/tools.h"
+
+//
+// class declaration
+//
+
+class skimEvents : public edm::stream::EDFilter<> {
+  public:
+    explicit skimEvents(const edm::ParameterSet&);
+    ~skimEvents();
+
+    static void fillDescriptions(edm::ConfigurationDescriptions& descriptions);
+
+  private:
+    virtual void beginStream(edm::StreamID) override;
+    virtual bool filter(edm::Event&, const edm::EventSetup&) override;
+    virtual void endStream() override;
+
+    //virtual void beginRun(edm::Run const&, edm::EventSetup const&) override;
+    //virtual void endRun(edm::Run const&, edm::EventSetup const&) override;
+    //virtual void beginLuminosityBlock(edm::LuminosityBlock const&, edm::EventSetup const&) override;
+    //virtual void endLuminosityBlock(edm::LuminosityBlock const&, edm::EventSetup const&) override;
+
+     // ----------member data ---------------------------
+    eventHistos m_allEvents;
+    edm::EDGetToken m_genEventInfoToken;
+    edm::EDGetToken m_recoMuonToken;
+    edm::EDGetToken m_AK8recoJetsToken;
+    edm::EDGetToken m_metToken;
+};
+
+//
+// constants, enums and typedefs
+//
+
+//
+// static data member definitions
+//
+
+//
+// constructors and destructor
+//
+skimEvents::skimEvents(const edm::ParameterSet& iConfig) :
+  m_genEventInfoToken (consumes<GenEventInfoProduct> (iConfig.getParameter<edm::InputTag>("genInfo"))),
+  m_recoMuonToken (consumes<std::vector<pat::Muon>> (iConfig.getParameter<edm::InputTag>("recoMuons"))),
+  m_AK8recoJetsToken (consumes<std::vector<pat::Jet>> (iConfig.getParameter<edm::InputTag>("AK8recoJets"))),
+  m_metToken (consumes<std::vector<pat::MET>> (iConfig.getParameter<edm::InputTag>("met")))  
+{
+  //now do what ever initialization is needed
+  edm::Service<TFileService> fs;
+  m_allEvents.book((fs->mkdir("allEvents")), 1);
+
+}
+
+
+skimEvents::~skimEvents()
+{
+ 
+   // do anything here that needs to be done at destruction time
+   // (e.g. close files, deallocate resources etc.)
+
+}
+
+
+//
+// member functions
+//
+
+// ------------ method called on each new Event  ------------
+bool
+skimEvents::filter(edm::Event& iEvent, const edm::EventSetup& iSetup)
+{
+  eventBits myRECOevent;
+
+  edm::Handle<GenEventInfoProduct> eventInfo;
+  iEvent.getByToken(m_genEventInfoToken, eventInfo);
+  myRECOevent.weight = eventInfo->weight();
+  std::cout <<"THIS EVENT HAS A WEIGHT OF: "<<eventInfo->weight() <<std::endl;
+  
+  edm::Handle<std::vector<pat::Muon>> recoMuons;
+  iEvent.getByToken(m_recoMuonToken, recoMuons);
+
+  edm::Handle<std::vector<pat::Jet>> ak8recoJets;
+  iEvent.getByToken(m_AK8recoJetsToken, ak8recoJets);
+
+
+  return true;
+}
+
+// ------------ method called once each stream before processing any runs, lumis or events  ------------
+void
+skimEvents::beginStream(edm::StreamID)
+{
+}
+
+// ------------ method called once each stream after processing all runs, lumis and events  ------------
+void
+skimEvents::endStream() {
+}
+
+// ------------ method called when starting to processes a run  ------------
+/*
+void
+skimEvents::beginRun(edm::Run const&, edm::EventSetup const&)
+{ 
+}
+*/
+ 
+// ------------ method called when ending the processing of a run  ------------
+/*
+void
+skimEvents::endRun(edm::Run const&, edm::EventSetup const&)
+{
+}
+*/
+ 
+// ------------ method called when starting to processes a luminosity block  ------------
+/*
+void
+skimEvents::beginLuminosityBlock(edm::LuminosityBlock const&, edm::EventSetup const&)
+{
+}
+*/
+ 
+// ------------ method called when ending the processing of a luminosity block  ------------
+/*
+void
+skimEvents::endLuminosityBlock(edm::LuminosityBlock const&, edm::EventSetup const&)
+{
+}
+*/
+ 
+// ------------ method fills 'descriptions' with the allowed parameters for the module  ------------
+void
+skimEvents::fillDescriptions(edm::ConfigurationDescriptions& descriptions) {
+  //The following says we do not know what parameters are allowed so do no validation
+  // Please change this to state exactly what you do use, even if it is no parameters
+  edm::ParameterSetDescription desc;
+  desc.setUnknown();
+  descriptions.addDefault(desc);
+}
+//define this as a plug-in
+DEFINE_FWK_MODULE(skimEvents);
