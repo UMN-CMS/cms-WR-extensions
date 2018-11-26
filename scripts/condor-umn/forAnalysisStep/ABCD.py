77167e382ceb5bea94abef00ad61b309b01eb395
Michael David Krohn <michael.krohn@colorado.edu>
2018-11-26 16:11:04 -0600

    1 high pT Muon, combine updates

--- scripts/condor-umn/forAnalysisStep/ABCD.py
+++ scripts/condor-umn/forAnalysisStep/ABCD.py
@@ -47,6 +47,11 @@ QpartBerr = 0.925
 QpartCerr = 0.24
 
 def histFromStack(stack):
+    print "Merging N hists: " + str(stack.GetNhists())
+    for iHist in range(0, stack.GetNhists()):
+        print iHist
+        hist = stack.GetHists()[iHist]
+        hist.Sumw2()
     newHist = copy.deepcopy(stack.GetHists()[0])
     newHist.Scale(0.0)
     newHist.Merge(stack.GetHists())
@@ -115,6 +120,10 @@ if (mode == "STACK"):
     histoB = histFromStack(fileB.Get(plotNameB)) 
     histoC = histFromStack(fileC.Get(plotNameC)) 
 
+histoA.Sumw2()
+histoB.Sumw2()
+histoC.Sumw2()
+
 nBinsA = histoA.GetNbinsX()
 nBinsB = histoB.GetNbinsX()
 nBinsC = histoC.GetNbinsX()
@@ -135,6 +144,17 @@ for ibin in range(1,nBinsA+1) :
     integralA = integralA + histoA.GetBinContent(ibin) 
     integralB = integralB + histoB.GetBinContent(ibin) 
     integralC = integralC + histoC.GetBinContent(ibin) 
+    "ErrDiff:"
+    errSqrA = abs (histoA.GetBinContent(ibin) ) ** 0.5
+    errA = histoA.GetBinError(ibin)
+    if ( errA != errSqrA ): print "Sqrt: "+str(errSqrA)+" vs Err: "+str(errA)
+    errSqrB = abs (histoB.GetBinContent(ibin) ) ** 0.5
+    errB = histoB.GetBinError(ibin)
+    if ( errB != errSqrB ): print "Sqrt: "+str(errSqrB)+" vs Err: "+str(errB)
+    errSqrC = abs (histoC.GetBinContent(ibin) ) ** 0.5
+    errC = histoC.GetBinError(ibin)
+    if ( errC != errSqrC ): print "Sqrt: "+str(errSqrC)+" vs Err: "+str(errC)
+
     print "Integrals: "
     print integralA
     print integralB
@@ -144,6 +164,18 @@ print "underflow"
 print histoA.GetBinContent(0)
 print histoB.GetBinContent(0)
 print histoC.GetBinContent(0)
+print "entries"
+print histoA.GetEntries()
+print histoB.GetEntries()
+print histoC.GetEntries()
+print "sum of weights"
+print histoA.GetSumOfWeights()
+print histoB.GetSumOfWeights()
+print histoC.GetSumOfWeights()
+print "integrals"
+print histoA.Integral()
+print histoB.Integral()
+print histoC.Integral()
 
 if (integralA == 0.0 or integralB == 0.0 or integralC == 0.0):
     print "histogram with 0 for integral... cannot compute"
@@ -151,21 +183,45 @@ if (integralA == 0.0 or integralB == 0.0 or integralC == 0.0):
 
 #NOW TO CALCULATE THE RATIO
 
+histoArebin = copy.deepcopy(histoA)
+histoBrebin = copy.deepcopy(histoB)
+histoCrebin = copy.deepcopy(histoC)
+
+histoArebin.Rebin(nBinsA)
+histoBrebin.Rebin(nBinsB)
+histoCrebin.Rebin(nBinsC)
+
+print "A,B,C:"
+print str(histoArebin.GetBinContent(1)) + "+-" + str(histoArebin.GetBinError(1))
+print str(histoBrebin.GetBinContent(1)) + "+-" + str(histoBrebin.GetBinError(1))
+print str(histoCrebin.GetBinContent(1)) + "+-" + str(histoCrebin.GetBinError(1))
+
+dScale = ( histoBrebin.GetBinContent(1) * histoCrebin.GetBinContent(1) ) / histoArebin.GetBinContent(1)
+dErr = ( ( histoBrebin.GetBinError(1) * ( histoCrebin.GetBinContent(1) / histoArebin.GetBinContent(1) ) )**2.0 + ( histoCrebin.GetBinError(1) * ( histoBrebin.GetBinContent(1) / histoArebin.GetBinContent(1) ) )**2.0 + ( histoArebin.GetBinError(1) * ( ( histoBrebin.GetBinContent(1) * histoCrebin.GetBinContent(1) ) / ( histoArebin.GetBinContent(1) )**2.0 ) )**2.0 ) ** 0.5
+print "Estimate D :"
+print str(dScale) + "+-" + str(dErr)
+
+#NOW TO CALCULATE THE D DISTRIBUTION
 ratioHisto = copy.deepcopy(histoA)
 ratioHisto.Scale(0.0)
+ratioHisto.Sumw2()
 
+diScale = dScale / histoArebin.GetBinContent(1)
+diScaleErr = ( ( dErr * ( 1 / histoArebin.GetBinContent(1) ) )**2.0 + ( histoArebin.GetBinError(1) * ( dScale / ( histoArebin.GetBinContent(1) )**2.0 ) )**2.0 ) ** 0.5
 
 for ibin in range(1,nBinsA+1):  
-    if histoA.GetBinContent(ibin) == 0:
-        print "DIVISION BY ZERO NOT POSSIBLE... SKIPPING BIN "+str(ibin)
-        continue
-
-    newBinVal = ( histoB.GetBinContent(ibin) * histoC.GetBinContent(ibin) ) / histoA.GetBinContent(ibin)
-    newBinErr = ( ( histoB.GetBinError(ibin) * ( histoC.GetBinContent(ibin) / histoA.GetBinContent(ibin) ) )**2.0 + ( histoC.GetBinError(ibin) * ( histoB.GetBinContent(ibin) / histoA.GetBinContent(ibin) ) )**2.0 + ( histoA.GetBinError(ibin) * ( ( histoB.GetBinContent(ibin) * histoC.GetBinContent(ibin) ) / ( histoA.GetBinContent(ibin) )**2.0 ) )**2.0 ) ** 0.5
-    
+    newBinVal = diScale * histoA.GetBinContent(ibin)
+    newBinErr = ( (diScale * histoA.GetBinError(ibin)) ** 2 + (diScaleErr * histoA.GetBinContent(ibin)) ** 2 ) ** 0.5 
 
     ratioHisto.SetBinContent(ibin, newBinVal)
     ratioHisto.SetBinError(ibin, newBinErr)
+
+#THIS IS MATHEMATICALLY DIFFERENT
+#ratioHisto = copy.deepcopy(histoB)
+#
+#ratioHisto.Multiply(histoC)
+#ratioHisto.Divide(histoA)
+
 
 #DRAW TIME
 maxA = histoA.GetMaximum()
@@ -207,14 +263,8 @@ c.SaveAs(output+"_ABCD.png")
 ratioHisto.SaveAs(output+"_ABCD_D.root")
 ratioHisto.SaveAs(output+"_ABCD_D.png")
 
-histoA.Rebin(nBinsA)
-histoB.Rebin(nBinsB)
-histoC.Rebin(nBinsC)
+#DOUBLE CHECKING
 ratioHisto.Rebin(nBinsA)
 
-print "A,B,C:"
-print str(histoA.GetBinContent(1)) + "+-" + str(histoA.GetBinError(1))
-print str(histoB.GetBinContent(1)) + "+-" + str(histoB.GetBinError(1))
-print str(histoC.GetBinContent(1)) + "+-" + str(histoC.GetBinError(1))
-print "Estimates D:"
-print str(ratioHisto.GetBinContent(1)) + "+-" + str(ratioHisto.GetBinError(1))
+print "Estimate D integral: NOTE: uncertainty here is unreliable as the ratioHisto does not contain the bin entry weights of the original distributions"
+print str(ratioHisto.GetBinContent(1))+"+-"+str(ratioHisto.GetBinError(1))
