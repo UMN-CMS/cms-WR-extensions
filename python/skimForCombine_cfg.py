631c39bcb78abba7320cd1cd6948eb11aa93359a
Andrew Evans <aevans@physics.umn.edu>
2019-04-03 14:32:36 -0500

    more skimming

---
+++ python/skimForCombine_cfg.py
@@ -0,0 +1,61 @@
+import FWCore.ParameterSet.Config as cms
+from FWCore.ParameterSet.VarParsing import VarParsing
+
+options = VarParsing ('analysis')
+
+options.maxEvents = -1
+options.parseArguments()
+muonID =' userInt("highPtID") == 1'
+
+process = cms.Process("Demo")
+
+process.load('Configuration.StandardSequences.Services_cff')
+process.load('FWCore.MessageService.MessageLogger_cfi')
+process.load('Configuration.EventContent.EventContent_cff')
+process.load('Configuration.StandardSequences.GeometryRecoDB_cff')
+process.load('Configuration.StandardSequences.MagneticField_38T_cff')
+process.load('Configuration.StandardSequences.EndOfProcess_cff')
+process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_condDBv2_cff')
+
+process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(options.maxEvents) )
+
+process.source = cms.Source ("PoolSource",
+	  fileNames = cms.untracked.vstring (options.inputFiles),
+)
+
+process.options = cms.untracked.PSet(
+    SkipEvent = cms.untracked.vstring('ProductNotFound')
+)
+
+process.TFileService = cms.Service("TFileService", 
+                        fileName = cms.string(options.outputFile)
+)   #for MC
+
+process.skim = cms.EDFilter('skimEventsForCombine',
+                              recoMuons = cms.InputTag("slimmedMuons"),
+                              recoElectrons = cms.InputTag("slimmedElectrons"),
+                              AK8recoJets = cms.InputTag("slimmedJetsAK8"),
+                              AK4recoJets = cms.InputTag("slimmedJetsAK4"),
+                              genInfo = cms.InputTag("generator"),
+                              isMC = cms.untracked.bool(True),
+                              amcatnlo = cms.untracked.bool(False),
+                              doBoost = cms.untracked.bool(True),
+                              doRes = cms.untracked.bool(True)
+)
+process.skimOutput = cms.OutputModule("PoolOutputModule",
+        dataset = cms.untracked.PSet(
+            dataTier = cms.untracked.string('MINIAOD'),
+            filterName = cms.untracked.string('wrSkim')
+        ),
+        fileName = cms.untracked.string('file:skimmedOut.root'),
+
+        SelectEvents = cms.untracked.PSet(
+            SelectEvents = cms.vstring('p')
+
+        )
+)
+ 
+
+process.p = cms.Path(process.skim)
+process.out = cms.EndPath(process.skimOutput)
+process.schedule = cms.Schedule(process.p,process.out)
