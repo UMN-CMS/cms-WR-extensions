2e84e9e7dcc2fe289246797c20cc05b227230352
Michael David Krohn <michael.krohn@colorado.edu>
2019-06-04 10:56:13 -0500

    put back commented lines

--- scripts/condor-umn/forAnalysisStep/createAndSubmitJobsWithCrab3.py
+++ scripts/condor-umn/forAnalysisStep/createAndSubmitJobsWithCrab3.py
@@ -521,12 +521,12 @@ with open(localInputListFile, 'r') as f:
     #crabSubmit(config)
     # workaround for cmssw multiple-loading problem
     # submit in subprocess
-#    q = Queue()
-#    p = Process(target=crabSubmit, args=(config,))
-#    p.start()
-#    p.join()
-#    if q.get()==-1:
-#      exit(-1)
+    q = Queue()
+    p = Process(target=crabSubmit, args=(config,))
+    p.start()
+    p.join()
+    if q.get()==-1:
+      exit(-1)
     
 print 'Done!' 
 exit(0)
